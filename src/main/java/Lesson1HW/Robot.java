0e2759e82036730ea019d2aa43fa811d87e559e8
[Jane22] <[yana-222@yandex.ru]>
2021-03-27 02:11:08 +0300

    Lesson1HW

---
+++ src/main/java/Lesson1HW/Robot.java
@@ -0,0 +1,43 @@
+package Lesson1HW;
+//1. Создайте три класса Человек, Кот, Робот, которые не наследуются от одного класса.
+// Эти классы должны уметь бегать и прыгать (методы просто выводят информацию о действии в консоль).
+//2. Создайте два класса: беговая дорожка и стена, при прохождении через которые,
+// участники должны выполнять соответствующие действия (бежать или прыгать), результат выполнения печатаем в консоль
+// (успешно пробежал, не смог пробежать и т.д.).
+//3. Создайте два массива: с участниками и препятствиями, и заставьте всех участников пройти этот набор препятствий.
+//4. У препятствий есть длина (для дорожки) или высота (для стены), а участников ограничения на бег и прыжки.
+// Если участник не смог пройти одно из препятствий, то дальше по списку он препятствий не идет.
+
+public class Robot implements Actions{
+    String Name;
+    int MaxRan;
+    int MaxHeight;
+
+    public Robot(String name, int maxRan, int maxHeight) {
+        Name = name;
+        MaxRan = maxRan;
+        MaxHeight = maxHeight;
+    }
+
+    @Override
+    public void run(int length) {
+        System.out.println(Name + " robot has run "+ length + " metres");
+    }
+
+    @Override
+    public void jump(int height) {
+        System.out.println(Name + " robot has jumped on " + height + " meters height");
+    }
+
+    public String getName() {
+        return Name;
+    }
+
+    public int getMaxRan() {
+        return MaxRan;
+    }
+
+    public int getMaxHeight() {
+        return MaxHeight;
+    }
+}
