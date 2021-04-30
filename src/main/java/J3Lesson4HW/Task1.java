bc00e1e86a1b8cb0524e284a47899a54ad00e846
[Jane22] <[yana-222@yandex.ru]>
2021-04-30 10:05:12 +0300

    J3Lesson4HW-question

--- src/main/java/J3Lesson4HW/Task1.java
+++ src/main/java/J3Lesson4HW/Task1.java
@@ -6,35 +6,88 @@ package J3Lesson4HW;
 import java.io.IOException;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.concurrent.atomic.AtomicInteger;
 
 public class Task1 {
-    private final Object monitor = new Object();
-    char currentLetter = 'g';
-
-    public static void main(String[] args) {
+    private static final Object monitor = new Object();
+    static volatile  char letter ='A';
+    static char letterA = 'A';
+    static char letterB = 'B';
+    static char letterC = 'C';
+    public static void main(String[] args) throws InterruptedException {
         Task1 tas = new Task1();
-        synchronized (tas.monitor) {
+          //  tas.task(letterA,letterB);
+          //  tas.task(letterB, letterC);
+          //  tas.task(letterC, letterA);
+        new Thread(() -> {
             for (int i = 0; i < 5; i++) {
-                new Thread(() -> {
-                    tas.task("A");
-                }).start();
-
-                new Thread(() -> {
-                    tas.task("B");
-                }).start();
-
-                new Thread(() -> {
-                    tas.task("C");
-                }).start();
-                tas.monitor.notifyAll();
+                synchronized (monitor){
+                    while (letter != 'A') {
+                        try {
+                            tas.monitor.wait();
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                    System.out.print('A');
+                    letter = 'B';
+                    monitor.notifyAll();
+                }
             }
-        }
+        }).start();
+        new Thread(() -> {
+            for (int i = 0; i < 5; i++) {
+                synchronized (monitor){
+                    while (letter != 'B') {
+                        try {
+                            tas.monitor.wait();
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                    System.out.print('B');
+                    letter = 'C';
+                    monitor.notifyAll();
+                }
+            }
+        }).start();
+        new Thread(() -> {
+            for (int i = 0; i < 5; i++) {
+                synchronized (monitor){
+                    while (letter != 'C') {
+                        try {
+                            tas.monitor.wait();
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                    System.out.print('C');
+                    letter = 'A';
+                    monitor.notifyAll();
+                }
+            }
+        }).start();
     }
 
-        public void task (String letter){
-             System.out.print(letter);
+    public void  task(char let, char next){
+            new Thread(() -> {
+                for (int i = 0; i < 5; i++) {
+                   synchronized (monitor){
+                        while (let != letter) {
+                            try {
+                                wait();
+                            } catch (InterruptedException e) {
+                                e.printStackTrace();
+                            }
+                        }
+                        System.out.print(let);
+                        letter = next;
+                        monitor.notifyAll();
+                    }
+                }
+            }).start();
         }
 
-    }
+ }
 
 
